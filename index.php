5aafa0bff1ab8167d76315aa80280d3cd8c1b7ae
root <tchenioguillaume@gmail.com>
2019-12-19 16:25:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'templating/header.php';
+?>
+
+<form action="#" method="post">
+  <center>
+    <input type="textarea" name="text">
+    <input type="submit" name="button" value="Valider">
+  </center>
+</form>
+
+<?php
+include 'templating/footer.php';
+?>
